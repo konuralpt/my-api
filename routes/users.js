91c54de0f8cfac83082ceddc1d3ed24f04c1adce
Konuralp Toksoy <konuralptoksoy4@gmail.com>
2019-08-25 19:12:56 +0300

    delete unnecessary files

--- routes/users.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const userController = require('../app/api/controllers/users');
-const validateJWT = require('../app/api/auth');
-
-router.post('/register', userController.create);
-router.post('/authenticate', userController.authenticate);
-
-module.exports = router;
